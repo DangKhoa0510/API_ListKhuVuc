ef6bfacdf91f51b26dfb53114257a89f78b3eacb
DangKhoa0510 <dangkhoanguyen0510@gmail.com>
2023-02-15 16:27:29 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Project
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://smart.cpc.vn/CISP_API/api/CISP/LoadLISTKHUVUC");
+            var content = new StringContent("{\r\n    \"TOKEN\":\"\"\r\n}", null, "application/json");
+            request.Content = content;
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
